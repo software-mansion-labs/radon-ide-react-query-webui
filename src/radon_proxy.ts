196bad679213c8b5f2e00d8b8b30109e2a56227c
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2025-12-03 15:14:23 +0100

    use flatted for serializing data

--- src/radon_proxy.ts
+++ src/radon_proxy.ts
@@ -1,9 +1,11 @@
+import * as flatted from "flatted";
+
 export interface ProxyClient {
-   sendMessage: (type: string, data?: any) => void;
-   addMessageListener: (method: string, listener: (params: any) => void) => void;
- }
- 
- export type ProxyClientFactory = () => Promise<ProxyClient>;
+  sendMessage: (type: string, data?: any) => void;
+  addMessageListener: (method: string, listener: (params: any) => void) => void;
+}
+
+export type ProxyClientFactory = () => Promise<ProxyClient>;
 
 // @ts-ignore
 const vscode = acquireVsCodeApi();
@@ -24,7 +26,7 @@ class RNIDEProxyClient implements ProxyClient {
       const payload = message.data;
       this.listeners
         .get(payload.type)
-        ?.forEach((listener) => listener(payload.data));
+        ?.forEach((listener) => listener(flatted.parse(payload.data)));
     }
   };
 
@@ -48,4 +50,5 @@ class RNIDEProxyClient implements ProxyClient {
   };
 }
 
-export const createRNIDEProxyClient = (scope: string) => new RNIDEProxyClient(scope);
+export const createRNIDEProxyClient = (scope: string) =>
+  new RNIDEProxyClient(scope);
